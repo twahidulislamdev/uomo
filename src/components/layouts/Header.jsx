573dc41669cd4d841d72302b005cdde21616541b
Twahidul Islam <twahidulislam2005@gmail.com>
2025-09-13 23:07:15 +0600

    refactor Header component to enhance menu functionality and improve layout responsiveness

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -8,11 +8,13 @@ import { FaRegUser, FaRegHeart } from "react-icons/fa";
 import { HiOutlineShoppingBag, HiMiniBars3CenterLeft } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import { GrClose } from "react-icons/gr";
+import { FaBars } from "react-icons/fa6";
 
 const Header = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isCtategoryOpen, setIsCategoryOpen] = useState(false);
   const [isUserOpen, setIsUserOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
@@ -83,30 +85,96 @@ const Header = () => {
         </Container>
       </div>
 
+      {/* -----------------------------============================= */}
       {/* Responsive Header Start  */}
-      <div className="lg:hidden">
+      <div className="block lg:hidden py-5 lg:py-0 mx-2 lg:mx-0">
         <Container>
           <Flex className={"justify-between items-center"}>
-            <div className="">
+            <div className="ml-5">
               <Image imgSrc={HeaderLogo} imgAlt={"Header Logo"} />
             </div>
             <div className="">
-              
+              <FaBars
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="text-4xl"
+              />
             </div>
           </Flex>
+          {isMenuOpen && (
+            <div className="ml-3 mt-5 bg-gray-200">
+              <ul className="text-center">
+                <Link to={"/"}>
+                  <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                    HOME
+                    <span
+                      className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                    ></span>
+                  </li>
+                </Link>
+                <Link to={"/shop"}>
+                  <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                    SHOP
+                    <span
+                      className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                    ></span>
+                  </li>
+                </Link>
+                <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                  BLOG
+                  <span
+                    className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                  ></span>
+                </li>
+                <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                  ABOUT
+                  <span
+                    className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                  ></span>
+                </li>
+                <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                  CONTACT
+                  <span
+                    className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                  ></span>
+                </li>
+                <li className="relative list-none px-2 py-2 text-sm  text-black  font-medium cursor-pointer group  hover:text-black  hover:cursor-pointer hover:translate-x-0 transition-all ease-in-out ">
+                  PAGES
+                  <span
+                    className="absolute left-0 bottom-0  h-0.5 bg-black transform  
+                      w-0 group-hover:w-full group-hover:left-0 group-hover:translate-x-0 
+                      transition-all duration-300 ease-in-out"
+                  ></span>
+                </li>
+              </ul>
+            </div>
+          )}
         </Container>
       </div>
       {/* Responsive Header End   */}
-
+      {/* -----------------------------============================= */}
       {/* Header Upper Part End  */}
       {/* --------------------================================================= */}
+      {/* --------------------================================================= */}
+
+      {/* --------------------================================================= */}
+      {/* --------------------================================================= */}
       {/* Header Lower Part Start  */}
-      <div className="py-5 px-2 lg:px-0 relative bg-[#F5F5F3] w-[96%] lg:w-full  ">
-        <Container className={"py-5"}>
+      <div className="py-5 px-2 lg:px-0 relative bg-[#F5F5F3] w-[98%] ml-3 lg:ml-0 lg:w-full  ">
+        <Container className={""}>
           <div className="flex justify-between items-center">
             <div className="flex justify-center items-center gap-1 gap-x-0 lg:gap-x-3 ">
               <HiMiniBars3CenterLeft
-                onClick={() => setIsMenuOpen(true)}
+                onClick={() => setIsCategoryOpen(true)}
                 className=" text-2xl   hover:cursor-pointer"
               />
             </div>
@@ -178,16 +246,16 @@ const Header = () => {
         {/* Cart Sidebar End <=  */}
 
         {/* Three Bar Sidebar Start =>  */}
-        {isMenuOpen && (
+        {isCtategoryOpen && (
           <div
             className={`absolute top-0 left-0 lg-full w-screen h-[695px] px-0 lg:px-3 py-0 lg:py-5 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
-              isMenuOpen ? "-translate-x-0" : "-translate-x-full"
+              isCtategoryOpen ? "-translate-x-0" : "-translate-x-full"
             }`}
           >
             {/* Menu Heading and Cross go here */}
             <div className="p-4 flex justify-between items-center ">
               <GrClose
-                onClick={() => setIsMenuOpen(false)}
+                onClick={() => setIsCategoryOpen(false)}
                 className="text-xl font-bold cursor-pointer"
               />
               <h4 className="text-xl font-medium">MENU</h4>
