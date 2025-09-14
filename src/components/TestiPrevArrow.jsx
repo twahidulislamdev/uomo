94e526c99473494d223d162eabe53e7a2ce61df2
Twahidul Islam <twahidulislam2005@gmail.com>
2025-09-14 11:16:15 +0600

    refactor TestiNextArrow and TestiPrevArrow components for improved layout; adjust right and left positioning for better alignment

--- src/components/TestiPrevArrow.jsx
+++ src/components/TestiPrevArrow.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowBack } from "react-icons/io";
 const TestiPrevArrow = ({onClick}) => {
   return (
     <div
-      className={`absolute top-[45%] left-5 lg:left-[50px] z-10 transform -translate-y-1/2 cursor-pointer w-10 h-10 rounded-[50%] border-2 border-gray-500  `}
+      className={`absolute top-[45%] left-10 lg:left-[50px] z-10 transform -translate-y-1/2 cursor-pointer w-10 h-10 rounded-[50%] border-2 border-gray-500  `}
       onClick={onClick}
     >
       <IoIosArrowBack className="text-4xl text-gray-500 rounded-full p-1 shadow" />
