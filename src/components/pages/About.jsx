f0e1901bf69ba6916a6dc4db2783f6e15a084953
Twahidul Islam <twahidulislam2005@gmail.com>
2026-01-16 23:50:55 +0600

    Add some text

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,8 +7,17 @@ const About = () => {
     <>
       <div className="">
         <Container>
-          <h6 className="text-3xl font-bold uppercase pt-5">About oumo</h6>
+          <h5 className="text-3xl font-bold uppercase pt-5">About oumo</h5>
           <img className="w-full h-full mt-5 " src={AboutOne} alt="" />
+          <h6 className="mt-10 text-3xl font-semibold">OUR STORY</h6>
+          <p className="text-md">
+            <span >
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </span>
+          </p>
         </Container>
       </div>
     </>
