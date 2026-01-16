81ab61a8a771243cebea03ad3cde87ced3d3bc40
Twahidul Islam <twahidulislam2005@gmail.com>
2026-01-16 23:46:27 +0600

    Work start in About Section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Container from "../Container";
+import AboutOne from "../../assets/aboutOne.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="">
+        <Container>
+          <h6 className="text-3xl font-bold uppercase pt-5">About oumo</h6>
+          <img className="w-full h-full mt-5 " src={AboutOne} alt="" />
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default About;
